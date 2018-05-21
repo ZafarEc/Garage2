ce2dec8ff2f38cd3ca7f5dcf0c8cccbcda85e730
zafar iqbal <rasalunic@gmail.com>
2018-05-21 11:30:55 +0200

    CreateModel Class

---
+++ Garage2Mvc/Garage2Mvc/Models/ParkedVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2Mvc.Models
+{
+    public class ParkedVehicle
+    {
+        public string Type { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You have to have a registration number")]
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+
+
+    }
+}
