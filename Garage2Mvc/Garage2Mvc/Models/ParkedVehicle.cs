68eba580486bb36c20933384cbdb47cb19d9d87c
zafar iqbal <rasalunic@gmail.com>
2018-05-21 11:51:11 +0200

    update model class

--- Garage2Mvc/Garage2Mvc/Models/ParkedVehicle.cs
+++ Garage2Mvc/Garage2Mvc/Models/ParkedVehicle.cs
@@ -8,12 +8,20 @@ namespace Garage2Mvc.Models
 {
     public class ParkedVehicle
     {
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You have to have a vehicle Type")]
+        [StringLength (15, MinimumLength = 2, ErrorMessage = "A name must be between 2 and 15 letters long")]
         public string Type { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "You have to have a registration number")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "A name must be between 4 and 8 letters long")]
         public string RegistrationNumber { get; set; }
+
+
         public string Color { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
+        [Range(2,12, ErrorMessage = "This not a valid wheel number")]
         public int NumberOfWheels { get; set; }
 
 
